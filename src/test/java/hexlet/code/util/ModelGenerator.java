25ed3c631f12b282c2e0e2f78ebf00e37d32cd52
Irina Yukavskaya <strix1813@yandex.ru>
2024-10-28 18:18:35 +0300

    add ModelGenerator

---
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -0,0 +1,33 @@
+package hexlet.code.util;
+
+import hexlet.code.model.User;
+import jakarta.annotation.PostConstruct;
+import lombok.Getter;
+import net.datafaker.Faker;
+import org.instancio.Instancio;
+import org.instancio.Model;
+import org.instancio.Select;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Component
+public class ModelGenerator {
+    private Model<User> userModel;
+
+//    @Autowired
+//    private Faker faker;
+    private final Faker faker = new Faker();
+
+    @PostConstruct
+    private void init() {
+        userModel = Instancio.of(User.class)
+                .ignore(Select.field(User::getId))
+                .supply(Select.field(User::getEmail), () -> faker.internet().emailAddress())
+                .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
+                .supply(Select.field(User::getLastName), () -> faker.name().lastName())
+                .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 100))
+//                .ignore(Select.field(User::getCreatedAt))
+//                .ignore(Select.field(User::getUpdatedAt))
+                .toModel();
+    }
+}
