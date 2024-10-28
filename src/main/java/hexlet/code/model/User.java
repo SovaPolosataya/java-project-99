63b0a22ca87e77fc2c9f2628688e1d2d24e4eba3
Irina Yukavskaya <strix1813@yandex.ru>
2024-10-28 18:28:04 +0300

    updating with UserDetails

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -14,8 +14,12 @@ import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -25,7 +29,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
@@ -34,15 +38,51 @@ public class User {
     private String firstName;
     private String lastName;
 
+    @NotNull
     @Email
     private String email;
 
-    @NotNull
+//    @NotNull
     @Size(min = 3)
-    private String password;
+    private String passwordDigest;
 
     @CreatedDate
     private LocalDate createdAt;
     @LastModifiedDate
-    private LocalDate updateAt;
+    private LocalDate updatedAt;
+
+    @Override
+    public String getPassword() {
+        return passwordDigest;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return new ArrayList<GrantedAuthority>();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 }
