ac49c701edc065057c3a2884048558205a36aa36
Irina Yukavskaya <strix1813@yandex.ru>
2024-10-30 00:30:03 +0300

    added implements BaseEntity, UserDetails

--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -1,12 +1,13 @@
 package hexlet.code.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -29,7 +30,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "users")
-public class User implements UserDetails {
+public class User implements BaseEntity, UserDetails {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
@@ -38,11 +39,11 @@ public class User implements UserDetails {
     private String firstName;
     private String lastName;
 
-    @NotNull
+    @Column(unique = true)
     @Email
     private String email;
 
-//    @NotNull
+//    @NotBlank
     @Size(min = 3)
     private String passwordDigest;
 
