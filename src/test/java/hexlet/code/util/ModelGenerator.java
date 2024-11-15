03b14e0a677dbd2669ad6965525016126a16c75c
Irina Yukavskaya <strix1813@yandex.ru>
2024-11-15 16:02:29 +0300

    added taskModel and labelModel

--- src/test/java/hexlet/code/util/ModelGenerator.java
+++ src/test/java/hexlet/code/util/ModelGenerator.java
@@ -1,7 +1,10 @@
 package hexlet.code.util;
 
+import hexlet.code.model.Label;
+import hexlet.code.model.Task;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import net.datafaker.Faker;
@@ -15,9 +18,9 @@ import org.springframework.stereotype.Component;
 public class ModelGenerator {
     private Model<User> userModel;
     private Model<TaskStatus> taskStatusModel;
+    private Model<Task> taskModel;
+    private Model<Label> labelModel;
 
-//    @Autowired
-//    private Faker faker;
     private final Faker faker = new Faker();
 
     @PostConstruct
@@ -28,14 +31,24 @@ public class ModelGenerator {
                 .supply(Select.field(User::getFirstName), () -> faker.name().firstName())
                 .supply(Select.field(User::getLastName), () -> faker.name().lastName())
                 .supply(Select.field(User::getPasswordDigest), () -> faker.internet().password(3, 100))
-//                .ignore(Select.field(User::getCreatedAt))
-//                .ignore(Select.field(User::getUpdatedAt))
                 .toModel();
 
         taskStatusModel = Instancio.of(TaskStatus.class)
                 .ignore(Select.field(TaskStatus::getId))
-                .supply(Select.field(TaskStatus::getName), () -> faker.name().title())
+                .supply(Select.field(TaskStatus::getName), () -> faker.name().name())
                 .supply(Select.field(TaskStatus::getSlug), () -> faker.name().title())
+                .toModel();
+
+        taskModel = Instancio.of(Task.class)
+                .ignore(Select.field(Task::getId))
+                .supply(Select.field(Task::getName), () -> faker.name().title())
+                .supply(Select.field(Task::getIndex), () -> faker.number().numberBetween(1, 10000))
+                .supply(Select.field(Task::getDescription), () -> faker.shakespeare().hamletQuote())
+                .toModel();
+
+        labelModel = Instancio.of(Label.class)
+                .ignore(Select.field(Label::getId))
+                .supply(Select.field(Label::getName), () -> faker.name().title())
                 .toModel();
     }
 }
