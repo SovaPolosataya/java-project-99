9d3e58edf639914fa30405ecfc3ff1a4501dc0e6
Irina Yukavskaya <strix1813@yandex.ru>
2024-11-15 16:05:43 +0300

    fixed

--- src/main/java/hexlet/code/service/UserService.java
+++ src/main/java/hexlet/code/service/UserService.java
@@ -1,13 +1,13 @@
 package hexlet.code.service;
 
-import hexlet.code.dto.UserCreateDTO;
-import hexlet.code.dto.UserDTO;
-import hexlet.code.dto.UserUpdateDTO;
+import hexlet.code.dto.user.UserCreateDTO;
+import hexlet.code.dto.user.UserDTO;
+import hexlet.code.dto.user.UserUpdateDTO;
 import hexlet.code.exception.ResourceNotFoundException;
 import hexlet.code.mapper.UserMapper;
 import hexlet.code.model.User;
 import hexlet.code.repository.UserRepository;
-import hexlet.code.util.UserUtils;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,11 +21,9 @@ public class UserService {
     @Autowired
     private UserMapper userMapper;
 
-    @Autowired
-    private UserUtils userUtils;
-
     public List<UserDTO> getAll() {
         List<User> users = userRepository.findAll();
+
         List<UserDTO> userDTOS = users.stream()
                 .map(userMapper::map)
                 .toList();
@@ -35,6 +33,7 @@ public class UserService {
     public UserDTO create(UserCreateDTO createDTO) {
         User user = userMapper.map(createDTO);
         userRepository.save(user);
+
         var userDTO = userMapper.map(user);
         return userDTO;
     }
@@ -42,6 +41,7 @@ public class UserService {
     public UserDTO findById(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User with id=" + id + " not found!"));
+
         UserDTO dto = userMapper.map(user);
         return dto;
     }
@@ -51,6 +51,7 @@ public class UserService {
                 .orElseThrow(() -> new ResourceNotFoundException("User with id=" + id + " not found!"));
         userMapper.update(userData, user);
         userRepository.save(user);
+
         var userDTO = userMapper.map(user);
         return userDTO;
     }
