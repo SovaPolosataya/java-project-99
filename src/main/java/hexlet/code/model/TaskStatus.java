a20e7684d21e1d92a86c918acf74c8542ff801b7
Irina Yukavskaya <strix1813@yandex.ru>
2024-11-15 16:03:12 +0300

    added OneToMany

--- src/main/java/hexlet/code/model/TaskStatus.java
+++ src/main/java/hexlet/code/model/TaskStatus.java
@@ -1,20 +1,24 @@
 package hexlet.code.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -24,19 +28,28 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "task_statuses")
+@NoArgsConstructor
 public class TaskStatus implements BaseEntity {
     @Id
     @GeneratedValue(strategy = IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotNull
+    @OneToMany(mappedBy = "taskStatus", cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    private List<Task> tasks = new ArrayList<>();
+
+    @Column(unique = true, nullable = false)
     private String name;
 
-    @NotBlank
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
+    @EqualsAndHashCode.Include
     private String slug;
 
     @CreatedDate
     private LocalDate createdAt;
+
+    public TaskStatus(String name, String slug) {
+        this.name = name;
+        this.slug = slug;
+    }
 }
