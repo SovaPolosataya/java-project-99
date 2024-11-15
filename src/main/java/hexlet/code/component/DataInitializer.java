a78b204b9583527fcb5cb3f3923389bfac2c7d57
Irina Yukavskaya <strix1813@yandex.ru>
2024-11-15 15:49:22 +0300

    added labels and statuses

--- src/main/java/hexlet/code/component/DataInitializer.java
+++ src/main/java/hexlet/code/component/DataInitializer.java
@@ -1,9 +1,12 @@
 package hexlet.code.component;
 
+import hexlet.code.model.Label;
 import hexlet.code.model.TaskStatus;
 import hexlet.code.model.User;
+import hexlet.code.repository.LabelRepository;
 import hexlet.code.repository.TaskStatusRepository;
 import hexlet.code.service.CustomUserDetailsService;
+
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -18,34 +21,24 @@ public class DataInitializer implements ApplicationRunner {
     private CustomUserDetailsService userService;
     @Autowired
     private TaskStatusRepository statusRepository;
+    @Autowired
+    private LabelRepository labelRepository;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        var email = "hexlet@example.com";
-        var userData = new User();
+        String email = "hexlet@example.com";
+        User userData = new User();
         userData.setEmail(email);
         userData.setPasswordDigest("qwerty");
         userService.createUser(userData);
 
-        var ts1 = new TaskStatus();
-        ts1.setName("Draft");
-        ts1.setSlug("draft");
-        statusRepository.save(ts1);
-        var ts2 = new TaskStatus();
-        ts2.setName("To review");
-        ts2.setSlug("to_review");
-        statusRepository.save(ts2);
-        var ts3 = new TaskStatus();
-        ts3.setName("To be fixed");
-        ts3.setSlug("to_be_fixed");
-        statusRepository.save(ts3);
-        var ts4 = new TaskStatus();
-        ts4.setName("To publish");
-        ts4.setSlug("to_publish");
-        statusRepository.save(ts4);
-        var ts5 = new TaskStatus();
-        ts5.setName("Published");
-        ts5.setSlug("published");
-        statusRepository.save(ts5);
+        statusRepository.save(new TaskStatus("Draft", "draft"));
+        statusRepository.save(new TaskStatus("To review", "to_review"));
+        statusRepository.save(new TaskStatus("To be fixed", "to_be_fixed"));
+        statusRepository.save(new TaskStatus("To publish", "to_publish"));
+        statusRepository.save(new TaskStatus("Published", "published"));
+
+        labelRepository.save(new Label("feature"));
+        labelRepository.save(new Label("bug"));
     }
 }
